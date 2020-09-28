21a057c2ce83cdef1c3454cfebf1a082c6832283
omermadmon <omermadmon@gmail.com>
2020-09-28 21:58:34 +0000

    add a method to retrieve a (real) board state for db.

--- src/main/java/com/google/research/bleth/services/DatabaseService.java
+++ src/main/java/com/google/research/bleth/services/DatabaseService.java
@@ -3,6 +3,9 @@ package com.google.research.bleth.services;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.research.bleth.simulator.Board;
 
 public class DatabaseService {
 
@@ -40,5 +43,21 @@ public class DatabaseService {
 
         // Write to datastore.
         datastore.put(boardStateEntity);
+    }
+
+    public String getRealBoardState(String simulationId, String round) {
+
+        Query boardStateBySimulationAndRoundQuery = new Query("TracingRealBoardState")
+                .setFilter(new Query.FilterPredicate("simulationId", Query.FilterOperator.EQUAL, simulationId))
+                .setFilter(new Query.FilterPredicate("round", Query.FilterOperator.EQUAL, round));
+
+        PreparedQuery pq = datastore.prepare(boardStateBySimulationAndRoundQuery);
+        Entity boardStateEntity = pq.asSingleEntity();
+
+        if (boardStateEntity != null) {
+            return (String) boardStateEntity.getProperty("state");
+        }
+
+        else return new Board(5, 5).getState();
     }
 }
