43e1974d992ce2ebf68f271098597bcf47a4865b
omermadmon <omermadmon@gmail.com>
2020-09-28 22:54:23 +0000

    remove redirect to resolve invalid json format error.

--- src/main/java/com/google/research/bleth/servlets/ReadHardCodedRealBoardStateServlet.java
+++ src/main/java/com/google/research/bleth/servlets/ReadHardCodedRealBoardStateServlet.java
@@ -1,7 +1,6 @@
 package com.google.research.bleth.servlets;
 
 import com.google.research.bleth.services.DatabaseService;
-import com.google.research.bleth.simulator.Board;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,17 +11,11 @@ import java.io.IOException;
 @WebServlet("/read-hard-coded-real-board-state")
 public class ReadHardCodedRealBoardStateServlet extends HttpServlet {
 
-    private Board board;
-
-    @Override
-    public void init() {
-
-    }
-
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json;");
-        String randomBoardState = DatabaseService.getInstance().getRandomRealBoardState();
+
+        String randomBoardState = DatabaseService.getInstance().getRealBoardState("1", "1");
         response.getWriter().println(randomBoardState);
     }
 }
