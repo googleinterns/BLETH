5429b7ec1f0eb32d0baa2756013be8f649c5b5e5
Orpaz Fishel Sheleg <orpazf@google.com>
2020-09-30 07:31:11 +0000

    Add the interface IObserver.

---
+++ src/main/java/com/google/research/bleth/simulator/IObserver.java
@@ -0,0 +1,16 @@
+package com.google.research.bleth.simulator;
+
+/** Represent an observer. */
+public interface IObserver extends Agent {
+
+    /**
+     * Add a transmission to the transmissions the observer observed so far in the current round.
+     * @param beaconTransmission the new transmission the observer observe.
+     */
+    void observe(Transmission beaconTransmission);
+
+    /**
+     * Deliver all the transmissions the observer observed in the current round to its resolver.
+     */
+    void passInformationToResolver();
+}
