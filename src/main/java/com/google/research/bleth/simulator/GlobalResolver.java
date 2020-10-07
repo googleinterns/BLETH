c8dd46c8877e876d4f2761ea85d5dbd3cbc8dca1
omermadmon <omermadmon@gmail.com>
2020-10-07 12:33:24 +0000

    add row and col number attributes to global resolver.

--- src/main/java/com/google/research/bleth/simulator/GlobalResolver.java
+++ src/main/java/com/google/research/bleth/simulator/GlobalResolver.java
@@ -3,6 +3,13 @@ package com.google.research.bleth.simulator;
 import java.util.List;
 
 public class GlobalResolver implements IGlobalResolver {
+    private final int rowNum;
+    private final int colNum;
+
+    public GlobalResolver(int rowNum, int colNum) {
+        this.rowNum = rowNum;
+        this.colNum = colNum;
+    }
 
     @Override
     public void receiveInformation(Location observerLocation, List<Transmission> transmissions) {
