66a3b5d0019e3dc75a21639deab245cdffce9676
omermadmon <omermadmon@gmail.com>
2020-10-07 13:36:00 +0000

    add a factory for generating awakeness strategies (no implementation).

---
+++ src/main/java/com/google/research/bleth/simulator/AwakenessStrategyFactory.java
@@ -0,0 +1,9 @@
+package com.google.research.bleth.simulator;
+
+/**
+ * A class for generating awakeness strategies based on awakeness cycle and duration.
+ * The factory gets as an argument the specific type of strategy to generate.
+ * The factory sets these constants for each generated strategy, and generates random parameters (if such exists).
+ */
+public class AwakenessStrategyFactory {
+}
