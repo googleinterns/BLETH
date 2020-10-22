3adfcb3cd7b464c0979568bacb78276ce9999bd2
omermadmon <omermadmon@gmail.com>
2020-10-22 19:19:51 +0000

    add placeholder classes (for testing database service class).

---
+++ src/main/java/com/google/research/bleth/simulator/AbstractSimulation.java
@@ -0,0 +1,33 @@
+package com.google.research.bleth.simulator;
+
+/** A placeholder class for representing a BLETH simulation. */
+public abstract class AbstractSimulation {
+
+    /** A placeholder class for representing a BLETH simulation builder. */
+    public static abstract class Builder {
+
+        public abstract String getSimulationType();
+
+        public int getMaxNumberOfRounds() { return 1; }
+
+        public int getRowNum() { return 1; }
+
+        public int getColNum() { return 1; }
+
+        public int getBeaconsNum() { return 1; }
+
+        public int getObserversNum() { return 1; }
+
+        public IMovementStrategy getBeaconMovementStrategy() { return null; }
+
+        public IMovementStrategy getObserverMovementStrategy() { return null; }
+
+        public AwakenessStrategyFactory.Type getAwakenessStrategyType() { return null; }
+
+        public double getRadius() { return 1; }
+
+        public int getAwakenessCycle() { return 1; }
+
+        public int getAwakenessDuration() { return 1; }
+    }
+}
---
+++ src/main/java/com/google/research/bleth/simulator/AwakenessStrategyFactory.java
@@ -0,0 +1,9 @@
+package com.google.research.bleth.simulator;
+
+/** A placeholder class for representing an awakeness strategy factory. */
+public class AwakenessStrategyFactory {
+
+    public enum Type {
+        FIXED, RANDOM;
+    }
+}
