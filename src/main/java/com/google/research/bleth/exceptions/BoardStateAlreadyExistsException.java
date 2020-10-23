d4e51dbcf9b5b24cb52a9beac203e0956aa56acf
omermadmon <omermadmon@gmail.com>
2020-10-23 16:45:30 +0000

    add exceptions javadocs.

--- src/main/java/com/google/research/bleth/exceptions/BoardStateAlreadyExistsException.java
+++ src/main/java/com/google/research/bleth/exceptions/BoardStateAlreadyExistsException.java
@@ -1,5 +1,6 @@
 package com.google.research.bleth.exceptions;
 
+/** An exception class to indicate a simulation's board state in a certain round have been already recorded on ab. . */
 public class BoardStateAlreadyExistsException extends RuntimeException {
     public BoardStateAlreadyExistsException(String message) {
         super(message);
--- src/main/java/com/google/research/bleth/exceptions/ExceedingRoundException.java
+++ src/main/java/com/google/research/bleth/exceptions/ExceedingRoundException.java
@@ -1,5 +1,6 @@
 package com.google.research.bleth.exceptions;
 
+/** An exception class to indicate a provided round number exceeds a simulation's maximum number of rounds. */
 public class ExceedingRoundException extends RuntimeException {
     public ExceedingRoundException(String message) {
         super(message);
