29eb8b19e70ca93ff0675ab93d65ba10074c1e70
omermadmon <omermadmon@gmail.com>
2020-10-28 12:19:38 +0000

    mark fields as final.

--- src/main/java/com/google/research/bleth/simulator/Schema.java
+++ src/main/java/com/google/research/bleth/simulator/Schema.java
@@ -4,31 +4,31 @@ package com.google.research.bleth.simulator;
 public class Schema {
     /** A class providing a single access point for the SimulationMetadata entity schema. */
     public static class SimulationMetadata {
-        public static String entityKind = "SimulationMetadata";
-        public static String type = "type";
-        public static String roundsNum = "roundsNum";
-        public static String beaconsNum = "beaconsNum";
-        public static String observersNum = "observersNum";
-        public static String rowsNum = "rowsNum";
-        public static String colsNum = "colsNum";
-        public static String beaconMovementStrategy = "beaconMovementStrategy";
-        public static String observerMovementStrategy = "observerMovementStrategy";
-        public static String observerAwakenessStrategy = "observerAwakenessStrategy";
-        public static String radius = "radius";
-        public static String awakenessCycle = "awakenessCycle";
-        public static String awakenessDuration = "awakenessDuration";
-        public static String observersDensity = "observersDensity";
-        public static String awakenessRatio = "awakenessRatio";
+        public static final String entityKind = "SimulationMetadata";
+        public static final String type = "type";
+        public static final String roundsNum = "roundsNum";
+        public static final String beaconsNum = "beaconsNum";
+        public static final String observersNum = "observersNum";
+        public static final String rowsNum = "rowsNum";
+        public static final String colsNum = "colsNum";
+        public static final String beaconMovementStrategy = "beaconMovementStrategy";
+        public static final String observerMovementStrategy = "observerMovementStrategy";
+        public static final String observerAwakenessStrategy = "observerAwakenessStrategy";
+        public static final String radius = "radius";
+        public static final String awakenessCycle = "awakenessCycle";
+        public static final String awakenessDuration = "awakenessDuration";
+        public static final String observersDensity = "observersDensity";
+        public static final String awakenessRatio = "awakenessRatio";
     }
 
     /** A class providing a single access point for the RealBoardState / EstimatedBoardState entity schema. */
     public static class BoardState {
-        public static String entityKindReal = "RealBoardState";
-        public static String entityKindEstimated = "EstimatedBoardState";
-        public static String simulationId = "simulationId";
-        public static String round = "round";
-        public static String agentId = "agentId";
-        public static String rowNum = "rowNum";
-        public static String colNum = "colNum";
+        public static final String entityKindReal = "RealBoardState";
+        public static final String entityKindEstimated = "EstimatedBoardState";
+        public static final String simulationId = "simulationId";
+        public static final String round = "round";
+        public static final String agentId = "agentId";
+        public static final String rowNum = "rowNum";
+        public static final String colNum = "colNum";
     }
 }
