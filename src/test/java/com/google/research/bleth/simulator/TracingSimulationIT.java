b4aab734e18c2687454b8a6345e44a58de4ae008
Orpaz Fishel Sheleg <orpazf@google.com>
2020-11-02 14:51:38 +0000

    Remove findAgent.

--- src/test/java/com/google/research/bleth/simulator/TracingSimulationIT.java
+++ src/test/java/com/google/research/bleth/simulator/TracingSimulationIT.java
@@ -5,8 +5,6 @@ import static com.google.common.truth.Truth.assertThat;
 import com.google.appengine.api.datastore.dev.LocalDatastoreService;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import com.google.common.collect.Multimap;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,8 +27,7 @@ public class TracingSimulationIT {
     private static final double TRANSMISSION_THRESHOLD_RADIUS_EQUALS_ONE = 1.0;
     private static final int AWAKENESS_CYCLE_EQUALS_TWO = 2;
     private static final int AWAKENESS_DURATION_EQUALS_ONE = 1;
-    private static final AwakenessStrategyFactory.Type FIXES_AWAKENESS_STRATEGY_TYPE =
-            AwakenessStrategyFactory.Type.FIXED;
+    private static final AwakenessStrategyFactory.Type FIXES_AWAKENESS_STRATEGY_TYPE = AwakenessStrategyFactory.Type.FIXED;
 
     @Before
     public void setUp() {
@@ -55,42 +52,24 @@ public class TracingSimulationIT {
                 .build();
 
         // Find agents and calculate their expected location after a single round.
-        Location beaconInitialLocation = findAgent(simulation.getBoard(), "Beacon");
-        Location observerInitialLocation = findAgent(simulation.getBoard(), "Observer");
+        Location beaconInitialLocation = simulation.beacons.get(0).getLocation();
+        Location observerInitialLocation = simulation.observers.get(0).getLocation();
         Location beaconExpectedLocation = predictLocationAfterMoveUp(beaconInitialLocation);
         Location observerExpectedLocation = observerInitialLocation;
 
         // Run single-rounded simulation and find agents again.
         simulation.run();
-        Location beaconActualLocation = findAgent(simulation.getBoard(), "Beacon");
-        Location observerActualLocation = findAgent(simulation.getBoard(), "Observer");
+        Location beaconActualLocation = simulation.beacons.get(0).getLocation();
+        Location observerActualLocation = simulation.observers.get(0).getLocation();
 
         assertThat(beaconActualLocation).isEqualTo(beaconExpectedLocation);
         assertThat(observerActualLocation).isEqualTo(observerExpectedLocation);
     }
 
-    private Location findAgent(Board board, String type) {
-        Multimap<Location, IAgent> agentsOnLocations = board.agentsOnBoard();
-        for(int row = 0; row < board.getRowNum(); row++) {
-            for(int col = 0; col < board.getColNum(); col++) {
-                Location loc = Location.create(row, col);
-                for(IAgent agent : agentsOnLocations.get(loc)) {
-                    if (agent.getId() == 0 && agent.getType().equals(type)) {
-                        return loc;
-                    }
-                }
-            }
-        }
-        return null;
-    }
-
-    @After
-    public void tearDown() {
-        helper.tearDown();
-    }
-
     private Location predictLocationAfterMoveUp(Location location) {
-        if(location.row() == 0) return location;
+        if (location.row() == 0) {
+            return location;
+        }
         return Location.create(location.row() - 1, location.col());
     }
 }
