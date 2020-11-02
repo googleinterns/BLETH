74f4f13b94947b31bb896fed33fcb10c1cb6932e
Orpaz Fishel Sheleg <orpazf@google.com>
2020-11-02 15:04:49 +0000

    Move the non-constant variables to the test itself.

--- src/test/java/com/google/research/bleth/simulator/TracingSimulationIT.java
+++ src/test/java/com/google/research/bleth/simulator/TracingSimulationIT.java
@@ -2,10 +2,6 @@ package com.google.research.bleth.simulator;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import com.google.appengine.api.datastore.dev.LocalDatastoreService;
-import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
-import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -13,36 +9,29 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class TracingSimulationIT {
 
-    private final LocalServiceTestHelper helper =
-            new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig()
-                    .setAutoIdAllocationPolicy(LocalDatastoreService.AutoIdAllocationPolicy.SCATTERED));
-
     private static final IMovementStrategy MOVE_UP = new UpMovementStrategy();
     private static final IMovementStrategy STATIONARY = new StationaryMovementStrategy();
 
-    private static final int BOARD_DIMENSION_EQUALS_TWO = 2;
-    private static final int MAX_ROUNDS_EQUALS_TWO = 2;
-    private static final int NUMBER_OF_BEACONS_EQUALS_ONE = 1;
-    private static final int NUMBER_OF_OBSERVERS_EQUALS_ONE = 1;
     private static final double TRANSMISSION_THRESHOLD_RADIUS_EQUALS_ONE = 1.0;
     private static final int AWAKENESS_CYCLE_EQUALS_TWO = 2;
     private static final int AWAKENESS_DURATION_EQUALS_ONE = 1;
     private static final AwakenessStrategyFactory.Type FIXES_AWAKENESS_STRATEGY_TYPE = AwakenessStrategyFactory.Type.FIXED;
 
-    @Before
-    public void setUp() {
-        helper.setUp();
-    }
-
     @Test
     public void runSimulationSingleRoundVerifyAgentsLocations() {
+        int roundsNum = 2;
+        int rowsNum = 2;
+        int colsNum = 2;
+        int beaconsNum = 1;
+        int observersNum = 1;
+
         // Create new simulation.
         AbstractSimulation simulation = new TracingSimulation.Builder()
-                .setMaxNumberOfRounds(MAX_ROUNDS_EQUALS_TWO)
-                .setRowNum(BOARD_DIMENSION_EQUALS_TWO)
-                .setColNum(BOARD_DIMENSION_EQUALS_TWO)
-                .setBeaconsNum(NUMBER_OF_BEACONS_EQUALS_ONE)
-                .setObserversNum(NUMBER_OF_OBSERVERS_EQUALS_ONE)
+                .setMaxNumberOfRounds(roundsNum)
+                .setRowNum(rowsNum)
+                .setColNum(colsNum)
+                .setBeaconsNum(beaconsNum)
+                .setObserversNum(observersNum)
                 .setTransmissionThresholdRadius(TRANSMISSION_THRESHOLD_RADIUS_EQUALS_ONE)
                 .setBeaconMovementStrategy(MOVE_UP)
                 .setObserverMovementStrategy(STATIONARY)
