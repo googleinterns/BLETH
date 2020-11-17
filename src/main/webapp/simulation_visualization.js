21886cb107178b3a95f02405900d7873f9920e44
omermadmon <omermadmon@gmail.com>
2020-11-17 08:51:54 +0000

    fetch board state foreach simulation round.

--- src/main/webapp/simulation_visualization.js
+++ src/main/webapp/simulation_visualization.js
@@ -4,6 +4,29 @@ window.visualize = visualize; // Add function to global scope.
 
 async function visualize() {
     var simulation = getUrlVars();
-    await sleep(3000);
-    console.log(simulation);
+    var header = document.getElementById('simulation-visualization-header');
+    header.innerText = 'Visualizing Simulation\n' + simulation.id;
+    var params = {};
+    var queryString;
+    params['simulationId'] = simulation.id;
+    for (var round = 0; round < simulation.roundsNum; round++) {
+        params['round'] = round;
+
+        // Request real board state.
+        params['isReal'] = true;
+        queryString = toQueryString(params);
+        fetch(`/read-board-state?${queryString}`)
+        .then(response => response.json())
+        .then(boardState => console.log(boardState.array));
+
+        // Request estimated board state.
+        params['isReal'] = false;
+        queryString = toQueryString(params);
+        fetch(`/read-board-state?${queryString}`)
+        .then(response => response.json())
+        .then(boardState => console.log(boardState.array));
+
+        // Delay.
+        await sleep(1000);
+    }
 }
