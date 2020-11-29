4b878c7ea738796c97f4355f1f7428923ea85976
omermadmon <omermadmon@gmail.com>
2020-11-29 20:10:55 +0000

    add exceptions handling.

--- src/main/java/com/google/research/bleth/servlets/DeleteSimulationServlet.java
+++ src/main/java/com/google/research/bleth/servlets/DeleteSimulationServlet.java
@@ -15,8 +15,13 @@ public class DeleteSimulationServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String simulationId = request.getParameter("simulationId");
-        Queries.delete(simulationId);
+        String responseText = "Simulation has been deleted successfully.";
+        try {
+            Queries.delete(simulationId);
+        } catch (Exception e) {
+            responseText = "Something went wrong: " + e.getMessage();
+        }
         response.setContentType("text/html;");
-        response.getWriter().println("Simulation has been deleted successfully.");
+        response.getWriter().println(responseText);
     }
 }
